4f5cb549e1dc7c5a896f1e4fd5e91469d6f3f506
Muheeb Pasha <muheebpasha0@gmail.com>
2026-01-29 19:10:32 +0530

    integrated custom hook i.e useFetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef,  useCallback, useEffect, useState } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
@@ -7,32 +7,23 @@ import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { fetchUserPlaces, updateUserPlaces } from './http.js';
 import Error from './components/Error.jsx';
+import useFetch from './hooks/useFetch.js';
 
 function App() {
   const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-
-  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
-  useEffect(()=>{
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({message : error.message || 'Failed to fetch user places.'})
-      }
-      setIsFetching(false); 
-    }
+  const { 
+          isFetching, 
+          fetchData: userPlaces, 
+          setFetchData: setUserPlaces,
+          error 
+        } = useFetch(fetchUserPlaces, []);
 
-    fetchPlaces();
-  },[]);
+
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -45,7 +36,6 @@ function App() {
   }
 
   async function handleSelectPlace(selectedPlace) {
-    //await updateUserPlaces([selectedPlace, ...userPlaces]);
 
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
@@ -80,7 +70,7 @@ function App() {
 
 
     setModalIsOpen(false);
-  }, [userPlaces]);
+  }, [userPlaces, setUserPlaces]);
 
   function handleError() {
     setErrorUpdatingPlaces(null);
@@ -125,7 +115,9 @@ function App() {
           />
         )}
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
+        <AvailablePlaces 
+                          onSelectPlace={handleSelectPlace} 
+                          />
       </main>
     </>
   );
